605bca19b7bcda50155cccc02daad696cc102526
Michael Panchenko <michael.panchenko@oraios-ai.de>
2025-03-30 23:56:52 +0200

    Minor refactoring in agno code, script starts

--- scripts/agno_agent.py
+++ scripts/agno_agent.py
@@ -1,12 +1,35 @@
-from agno.models.google import Gemini
+from agno.agent import Agent
+from agno.models.anthropic import Claude
 from agno.playground import Playground, serve_playground_app
+from dotenv import load_dotenv
 
-from serena.agno import create_agno_agent
+from serena.agent import SerenaAgent
+from serena.agno import get_agno_functions
+
+load_dotenv()
+
+project_file_path = "../myproject.yml"
+
+serena_agent = SerenaAgent(project_file_path)
+tools = get_agno_functions(serena_agent)
+
+model = Claude(id="claude-3-7-sonnet-20250219")
+
+agno_agent = Agent(
+    name="Serena",
+    model=model,
+    description="A fully-featured coding assistant",
+    tools=tools,  # type: ignore
+    show_tool_calls=True,
+    markdown=True,
+    system_message="",  # Todo
+    read_tool_call_history=True,
+    telemetry=False,
+)
+
+
+## The app object should be in the module scope so that the server can access it for hot reloading
+app = Playground(agents=[agno_agent]).get_app()
 
 if __name__ == "__main__":
-    serena_agent = create_agno_agent(
-        project_file_path="myproject.yml",
-        model=Gemini(id="gemini-2.5-pro-exp-03-25"),
-    )
-    app = Playground(agents=[serena_agent]).get_app()
-    serve_playground_app(app, reload=False)
+    serve_playground_app("agno_agent:app", reload=True)
--- src/serena/agno.py
+++ src/serena/agno.py
@@ -1,32 +1,18 @@
-from agno.agent import Agent
-from agno.models.base import Model
 from agno.tools import Function
 
-from serena.agent import SerenaAgent
+from serena.agent import SerenaAgent, Tool
 
 
-def create_agno_agent(
-    project_file_path: str, model: Model, system_prompt: str = "", markdown: bool = True, show_tool_calls: bool = True
-) -> Agent:
-    serena_agent = SerenaAgent(project_file_path)
-    tools: list[Function] = []
-    for tool in serena_agent.tools.values():
+def serena_tool_to_agno_function(tool: Tool) -> Function:
+    def entrypoint(*args, tool=tool, **kwargs):  # type: ignore
+        tool.apply_ex(*args, log_call=True, catch_exceptions=True, **kwargs)
 
-        def entrypoint(*args, tool=tool, **kwargs):  # type: ignore
-            tool.apply_ex(*args, log_call=True, catch_exceptions=True, **kwargs)
+    function = Function.from_callable(tool.get_apply_fn())
+    function.name = tool.get_name()
+    function.entrypoint = entrypoint
+    function.show_result = True
+    return function
 
-        function = Function.from_callable(tool.get_apply_fn())
-        function.name = tool.get_name()
-        function.entrypoint = entrypoint
-        tools.append(function)
 
-    agno_agent = Agent(
-        model=model,
-        description="Serena",
-        tools=tools,  # type: ignore
-        show_tool_calls=show_tool_calls,
-        markdown=markdown,
-        system_message=system_prompt,
-    )
-
-    return agno_agent
+def get_agno_functions(serena_agent: SerenaAgent) -> list[Function]:
+    return [serena_tool_to_agno_function(tool) for tool in serena_agent.tools.values()]
