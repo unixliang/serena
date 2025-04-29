6bc03191a714c00577a771153da3156a13014ba8
Michael Panchenko <michael.panchenko@oraios-ai.de>
2025-04-29 14:15:04 +0200

    Multilspy: sleep in TS language server before requesting references

--- src/multilspy/language_servers/gopls/gopls.py
+++ src/multilspy/language_servers/gopls/gopls.py
@@ -156,18 +156,16 @@ class Gopls(LanguageServer):
             await self.server.stop()
 
     @override
-    async def request_references(
-        self, relative_file_path: str, line: int, column: int
-    ) -> List[multilspy_types.Location]:
+    async def _send_references_request(self, relative_file_path: str, line: int, column: int) -> List[multilspy_types.Location]:
         NUM_COLS_TO_TRY = 10
-        """selectionRange in Gopls always return the wrong column, 
-        the one at the beginnning of the declaration instead of the symbol. We do a dirty hack here.
+        """selectionRange in Gopls always contains the wrong column, 
+        the one at the beginning of the declaration instead of the symbol. We do a dirty hack here (loop over some columns).
         See https://github.com/golang/go/issues/73521
         """
         actual_column = column
         for actual_column in range(column, column+NUM_COLS_TO_TRY):
             try:
-                return await super().request_references(relative_file_path, line, actual_column)
+                return await super()._send_references_request(relative_file_path, line, actual_column)
             except (Error, RuntimeError, MultilspyException) as e:
                 self.logger.log(f"Cannot find symbol at {line=}, column={actual_column}, trying to bump column by 1", logging.INFO)
-        raise RuntimeError(f"Failed to find references for symbol at {line=}, {column=}")
+        raise MultilspyException(f"Failed to find references for symbol in\n{relative_file_path=} at {line=}, {column=}")
