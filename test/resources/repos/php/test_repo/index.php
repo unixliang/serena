81b719241035fa8d87dd94ca6a313c394cfd653c
Michael Panchenko <michael.panchenko@unternehmertum.de>
2025-05-19 12:49:16 +0200

    Added tests for PhP language server

---
+++ test/resources/repos/php/test_repo/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'helper.php';
+
+function greet(string $name): string {
+    return "Hello, " . $name . "!";
+}
+
+$userName = "PHP User";
+$greeting = greet($userName);
+
+echo $greeting;
+
+helperFunction();
+
+?> 
