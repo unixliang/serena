605f8fba71cfd4c134ae568a45fdea5eb85241f0
Dominik Jain <dominik.jain@outlook.com>
2025-06-23 16:57:49 +0200

    Remove multilspy package

--- src/multilspy/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-This module contains the multilspy API
-"""
-
-from solidlsp import multilspy_types as Types
-from .language_server import LanguageServer, SyncLanguageServer
-
-__all__ = ["LanguageServer", "Types", "SyncLanguageServer"]
