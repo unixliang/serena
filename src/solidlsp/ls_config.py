31cee4da6341c6b221ea1943034a8b25083bbb8b
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2025-06-28 11:29:58 +0000

    Tell overeager codespell to stfu

--- src/solidlsp/ls_config.py
+++ src/solidlsp/ls_config.py
@@ -73,7 +73,7 @@ class Language(str, Enum):
             case self.PHP:
                 return FilenameMatcher("*.php")
             case self.CLOJURE:
-                return FilenameMatcher("*.clj", "*.cljs", "*.cljc", "*.edn")
+                return FilenameMatcher("*.clj", "*.cljs", "*.cljc", "*.edn")  # codespell:ignore edn
             case _:
                 raise ValueError(f"Unhandled language: {self}")
 
