851188876015e5ba9a526693df99c6440851d0a5
Dominik Jain <dominik.jain@oraios-ai.de>
2025-07-09 19:40:50 +0200

    Rename module ls_tools -> symbol_tools (They are no longer limited to LS-based tools, as the editing tools can use a different underlying CodeEditor)

--- src/serena/tools/__init__.py
+++ src/serena/tools/__init__.py
@@ -1,7 +1,7 @@
 # ruff: noqa
 from .tools_base import *
 from .file_tools import *
-from .ls_tools import *
+from .symbol_tools import *
 from .memory_tools import *
 from .cmd_tools import *
 from .config_tools import *
